d7a548a8ec163cf83aa8789a4e2a22e0c8ed0a97
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-08-14 12:31:00 +0530

    update 4

--- mart-ecommerce/src/components/Home/Home.jsx
+++ mart-ecommerce/src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Carousel from './Carousel';
+import Card1 from './Card1';
 
 const Home = () => {
   return (
     <div>
       <Carousel/>
+      <Card1 />
     </div>
   )
 }
